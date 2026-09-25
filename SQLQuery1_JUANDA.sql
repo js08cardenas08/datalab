a987f9df65d8a87a8a92a1b2b625bd8867cfabf5
JuanSegu230 <juangithub9@gmail.com>
2026-09-25 15:40:55 -0500

    Add files via upload
    Se sube el SQL server de la semana 6.2

---
+++ SQLQuery1_JUANDA.sql
@@ -0,0 +1,15 @@
+SELECT *
+FROM proyecto
+WHERE id_proyecto = 1;
+
+INSERT INTO proyecto
+    (nombre, descripcion)
+VALUES
+    ('Prediccion de demanda',
+     'Proyecto para analizar y predecir la demanda de servicios.');
+
+     SELECT *
+FROM proyecto
+WHERE nombre = 'Prediccion de demanda';
+
+
